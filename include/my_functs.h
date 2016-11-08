e59832133b7f810b7396f103ac80bcda94387af4
LyesKaidi <lyes.kaidi@gmail.com>
2016-11-08 22:09:50 +0100

    Create my_functs.h

---
+++ include/my_functs.h
@@ -0,0 +1,18 @@
+/*
+** ft_functs.h for ft_functs.h in /home/kiwi/Downloads/Evalexpr/include
+** 
+** Made by Lyes Kaïdi
+** Login   <kiwi@epitech.net>
+** 
+** Started on  Fri Oct 28 15:09:30 2016 Lyes Kaïdi
+** Last update Fri Oct 28 17:59:00 2016 Lyes Kaïdi
+*/
+
+#ifndef MY_FUNCTS_H_
+# define MY_FUNCTS_H_
+
+void	my_putchar(char c);
+void	my_putnbr(int nbr);
+int	my_atoi(char **s);
+
+#endif /* !MY_FUNCTS_H_ */
